358b4e4d991c9d699710e137688f55752262c80c
PinoStar <carlitosbriones24@gmail.com>
2025-01-07 16:52:36 -0500

    detallesproductos,catalogos coponentes

---
+++ frontend/app/src/app/home-vend/detalleproduc/detalleproduc.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalleproduc',
+  standalone: true,
+  imports: [],
+  templateUrl: './detalleproduc.component.html',
+  styleUrl: './detalleproduc.component.css'
+})
+export class DetalleproducComponent {
+
+}
